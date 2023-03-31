1892bfc5771eada4734358296c1bdf7050f9f416
Sima00000 <123153938+Sima00000@users.noreply.github.com>
2023-03-31 18:16:25 +0530

    commit

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useNavigate} from "react-router"
+import './Hero.scss'
+function Hero() {
+  const navigate = useNavigate();
+  return (
+    <div className='Hero'>
+      <div className="hero-content center">
+        <h2 className='heading'>Exclusive Print and ArtWork</h2>
+        <p className="subheading">Exclusive Art Pieces, for the Exclusive You.</p>
+      <button onClick={() => navigate('category')} className='cta btn-primary'>Explore more</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
